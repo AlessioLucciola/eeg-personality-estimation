Fixed triplet loss computation

models/vit.py
@@ -103,10 +103,10 @@ class ViT(nn.Module):
         x = self.encoder(x)
         #print(x.shape)
         if self.use_encoder_only:
-            x = x[:, 0, :]
+            feature = x[:, 0, :]
         else:
-            x = self.decoder(label_tokens, x)[:, 0, :]
+            feature = self.decoder(label_tokens, x)[:, 0, :]
         #print(x.shape)
-        x = self.classifier(x)
+        out = self.classifier(feature)
         #print(x.shape)
-        return x
+        return out, feature

train_modules/train_loops/train_loop_kfold_loo.py
@@ -132,12 +132,12 @@ def train_eval_loop(device,
                     tr_positive_labels = tr_positive_labels.to(device)
                     tr_negative_labels = tr_negative_labels.to(device)
                     
-                    tr_outputs = model(tr_data) # Prediction for the anchor sample
+                    tr_outputs, tr_features = model(tr_data) # Prediction for the anchor sample
                     epoch_tr_outputs = torch.cat((epoch_tr_outputs, tr_outputs), 0) # Concatenate the predictions for the anchor sample
-                    tr_positive_outputs = model(tr_positive_data) # Prediction for the positive sample
-                    tr_negative_outputs = model(tr_negative_data) # Prediction for the negative sample
+                    tr_positive_outputs, tr_positive_features = model(tr_positive_data) # Prediction for the positive sample
+                    tr_negative_outputs, tr_negative_features = model(tr_negative_data) # Prediction for the negative sample
 
-                    tr_triplet_loss = triplet_criterion(tr_outputs, tr_positive_outputs, tr_negative_outputs) # Compute the triplet loss
+                    tr_triplet_loss = triplet_criterion(tr_features, tr_positive_features, tr_negative_features) # Compute the triplet loss
                     tr_loss_anchor = binary_criterion(tr_outputs, tr_labels) # Compute the loss for the anchor sample
                     tr_loss_positive = binary_criterion(tr_positive_outputs, tr_positive_labels) # Compute the loss for the positive sample
                     tr_loss_negative = binary_criterion(tr_negative_outputs, tr_negative_labels) # Compute the loss for the negative sample
@@ -148,7 +148,7 @@ def train_eval_loop(device,
                     tr_labels = tr_labels.to(device)
 
                     # Forward pass
-                    tr_outputs = fold_model(tr_data) # Prediction
+                    tr_outputs, _ = fold_model(tr_data) # Prediction
                     epoch_tr_outputs = torch.cat((epoch_tr_outputs, tr_outputs), 0)
                     
                     # Loss computation
@@ -247,7 +247,7 @@ def train_eval_loop(device,
                     val_labels = val_labels.to(device)
 
                     # Forward pass
-                    val_outputs = fold_model(val_data)
+                    val_outputs, _ = fold_model(val_data)
                     epoch_val_outputs = torch.cat((epoch_val_outputs, val_outputs), 0)
 
                     # Loss computation